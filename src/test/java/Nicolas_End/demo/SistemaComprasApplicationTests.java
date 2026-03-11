7b5bca89ab24cd0aae4877553731698c2eae895d
Nicolas-End <nicolasvalsi203.com>
2026-03-11 20:59:29 -0300

    [feat] First Commit, inputing spring boot path

---
+++ src/test/java/Nicolas_End/demo/SistemaComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolas_End.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
