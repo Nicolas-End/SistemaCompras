77f727fca3472b0597f13d05df3c5e209ae1f0ec
Nicolas-End <nicolasvalsi203.com>
2026-05-04 20:29:35 -0300

    [fix] fixxing annotation import

--- src/main/java/Nicolas_End/demo/controllers/StaffController.java
+++ src/main/java/Nicolas_End/demo/controllers/StaffController.java
@@ -4,13 +4,10 @@ package Nicolas_End.demo.controllers;
 import Nicolas_End.demo.domains.staff.StaffService;
 import Nicolas_End.demo.dtos.staff.PostNewStaffDTO;
 import Nicolas_End.demo.infra.response.ApiResponse;
-import Nicolas_End.demo.infra.response.ResponseUtil;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/staff")
