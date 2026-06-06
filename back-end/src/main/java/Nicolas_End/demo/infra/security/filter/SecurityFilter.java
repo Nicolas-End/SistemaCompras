e109acb02ec53066af8d316039304c933d6581ba
Nicolas-End <nicolasvalsi203.com>
2026-06-06 18:53:27 -0300

    [fix] creating catch in filter to ExipredTokenException

--- back-end/src/main/java/Nicolas_End/demo/infra/security/filter/SecurityFilter.java
+++ back-end/src/main/java/Nicolas_End/demo/infra/security/filter/SecurityFilter.java
@@ -2,6 +2,7 @@ package Nicolas_End.demo.infra.security.filter;
 
 
 import Nicolas_End.demo.domains.staff.StaffRepository;
+import Nicolas_End.demo.infra.exception.CustomAuthenticationEntryPoint;
 import Nicolas_End.demo.infra.exception.ExpiredTokenException;
 import Nicolas_End.demo.infra.security.token.TokenService;
 import Nicolas_End.demo.infra.util.response.ResponseUtil;
@@ -9,6 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,34 +21,38 @@ import java.io.IOException;
 
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
     private final TokenService tokenService;
     private final StaffRepository staffRespository;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
 
-    public SecurityFilter(TokenService tokenService,
-                          StaffRepository staffRespository,
-                          ResponseUtil responseUtil){
-        this.tokenService = tokenService;
-        this.staffRespository = staffRespository;
-
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if(token != null){
-            var email = this.tokenService.validateToken(token);
-            UserDetails user = (UserDetails) staffRespository.findByEmail(email);
-            if(user == null){
-                throw new ExpiredTokenException();
-            }
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        try {
+            var token = this.recoverToken(request);
+            if (token != null) {
+                var email = this.tokenService.validateToken(token);
+                UserDetails user = (UserDetails) staffRespository.findByEmail(email);
+                if (user == null) {
+                    throw new ExpiredTokenException();
+                }
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 
-            // "segura os dados do usuario para poder utilizar durante a requisição
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                // "segura os dados do usuario para poder utilizar durante a requisição
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            filterChain.doFilter(request, response);
+        }catch (ExpiredTokenException e){
+            // evita de enviar o problema para o console do usuario
+            this.customAuthenticationEntryPoint.commence(
+                    request,
+                    response,
+                    new ExpiredTokenException()
+            );
         }
-        filterChain.doFilter(request,response);
     }
 
     private String recoverToken(HttpServletRequest request){
