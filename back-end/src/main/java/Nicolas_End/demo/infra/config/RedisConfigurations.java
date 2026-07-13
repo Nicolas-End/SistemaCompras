250117e11599d2cc1df608908cb1d1183e2b32c5
Nicolas-End <nicolasvalsi203@gmail.com>
2026-07-13 18:44:05 -0300

    [refactor] creating Redis connection test

--- back-end/src/main/java/Nicolas_End/demo/infra/config/RedisConfigurations.java
+++ back-end/src/main/java/Nicolas_End/demo/infra/config/RedisConfigurations.java
@@ -8,6 +8,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionCommands;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,10 +22,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class RedisConfigurations {
 
+    public RedisConfigurations(RedisTemplate redisTemplate){
+        this.redisTemplate = redisTemplate;
+    }
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public static final String ITEMS_CACHE = "items";
 
+    @Scheduled(initialDelay = 5000, fixedDelay = 60000)
+    public void validateRedisConnection() {
+        try {
+            String pong = redisTemplate.execute((RedisCallback<String>) RedisConnectionCommands::ping);
 
+            log.info("Redis conectado! Resposta: {}", pong);
+        } catch (Exception e) {
+            log.error("Erro ao conectar ao Redis", e);
+        }
+    }
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
